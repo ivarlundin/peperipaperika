d2f762d2becd5ce85fbc49c18320b8976c33040f
Ivar Lundin <60367191+ivarlundin@users.noreply.github.com>
2021-01-06 22:45:33 +0100

    Cool test yes

--- script.js
+++ script.js
@@ -1,3 +1,61 @@
+var particle = new Particle();
+var token = '573526f765ebc51cfd0e5f5469e1e56b5c17b47d';
+var buttonState = 0;
+
+function styleButton(){
+    if (buttonState == 1) {
+        document.getElementById("trigger").style.backgroundColor = "rgba(255, 255, 255, 0.8)";
+        document.getElementById("trigger").style.color = "rgb(77, 176, 15)";
+    } else {
+        document.getElementById("trigger").style.backgroundColor = "rgba(255, 255, 255, 0.2)";
+        document.getElementById("trigger").style.color = "rgb(62, 62, 62)";
+    }
+}
+styleButton();
+
+
+function turnOn() {
+    var fnPr = particle.callFunction({ deviceId: 'e00fce68d41921757c574eb6', name: 'led', argument: 'on', auth: token });
+    document.getElementById("trigger").innerHTML = "Turn Off";
+
+    fnPr.then(
+      function(data) {
+        console.log('Function called succesfully:', data);
+        buttonState = 1;
+        styleButton();
+        
+      }, function(err) {
+        console.log('An error occurred:', err);
+      }); 
+
+    return true;
+}
+
+function turnOff() {
+    var fnPr = particle.callFunction({ deviceId: 'e00fce68d41921757c574eb6', name: 'led', argument: 'off', auth: token });
+
+    document.getElementById("trigger").innerHTML = "Turn On";
+    fnPr.then(
+      function(data) {
+        console.log('Function called succesfully:', data);
+        buttonState = 0;
+        styleButton();
+
+      }, function(err) {
+        console.log('An error occurred:', err);
+      }); 
+
+    return true;
+}
+
+
+
+
+
+
+
+
+
 let text = "";
 let field = document.getElementById("specialText");
 let counter = 1;
@@ -42,7 +100,17 @@ document.addEventListener('keydown', function (e) {
         //console.log("BACK");
         text = field.innerHTML.slice(0, field.length - 1);
         console.log(text);
+        
 
         field.innerHTML = text;
     }
 }, false);
+
+document.getElementById("trigger").addEventListener("click", function(){ 
+    console.log("Click");
+    if (buttonState == 0) {
+        turnOn();
+    } else if (buttonState == 1) {
+        turnOff();
+    }
+});
